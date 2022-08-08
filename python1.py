978e0393dbc61e83d7131a2b742169bf3f56868c
ambonslot77 <110508888+zeusturu@users.noreply.github.com>
2022-08-07 18:51:18 -0700

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,12 @@
+#belajar Output
+
+print("hello world")
+print("nama saya ambon")
+print("saya wibu")
+print("ambon mulet")
+print("mcflury")
+print("alien cs")
+print("ez win")
+print("ambon jagoan")
+print("parang")
+print("gamon")
---
+++ python2.py
@@ -0,0 +1,14 @@
+#belajar input
+
+nama = "ambon" 
+umur = 15
+alamat = "Cimanggis, kota depok"
+
+print (nama)
+print (umur)
+print (alamat)
+
+print("nama saya adalah" + nama ) 
+
+print(nama saya adalah "+ nama + "umur saya" + str(umur) +  + " alamat saya  " + alamat )
+
